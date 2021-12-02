00ce6c7ce55c7e0a64a88de38126da97878df4d8
Bakytai Egemberdieva <bakijw@gmail.com>
2021-12-02 23:44:55 +0600

    создала все компоненты, настроила базовые настройки

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ContactsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule
